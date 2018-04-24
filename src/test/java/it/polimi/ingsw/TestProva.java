0c2344660569d15eec9167c33f5b5b33489d3877
Alberto Cicciarello <37806151+Albix1997@users.noreply.github.com>
2018-04-24 16:34:01 +0200

    Revert "Revert "Junit Fixato""
    This reverts commit 6099d865db257eb0c7f10b8822887c69364780a5.

---
+++ src/test/java/it/polimi/ingsw/TestProva.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestProva {
+    @Test
+    public void metodoProva(){
+        assertEquals(2,1+1);
+        
+    }
+}
