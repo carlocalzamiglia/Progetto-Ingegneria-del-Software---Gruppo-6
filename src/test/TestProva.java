fa073dd30b0fad5fa6a971e446db2146adfe49ae
carlocalzamiglia <carlo.calzamiglia@mail.polimi.it>
2018-05-11 16:09:32 +0200

    Update "pom"

--- src/test/TestProva.java
+++ src/test/TestProva.java
@@ -1,9 +1,7 @@
 import Game.*;
 
-import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestProva {
     @org.junit.jupiter.api.Test
