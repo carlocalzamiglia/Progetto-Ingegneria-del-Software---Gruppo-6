8059b48e53556f55ccb6c5302397aab4deada3b9
carlocalzamiglia <carlo.calzamiglia@mail.polimi.it>
2018-05-15 14:53:10 +0200

    Fix - folder hierarchy

---
+++ src/main/Java/it.polimi.ingsw/Client/ClientSetup.java
@@ -0,0 +1,29 @@
+package Client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+
+public class ClientSetup {
+    public static void main(String[]args) throws RemoteException {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Benvenuto, quale connessione vuoi utilizzare?");
+            System.out.println("Inserisci 1 per la connessione RMI ");
+            System.out.println("Inserisci 2 per la connessione Socket ");
+
+            int choose = in.nextInt();
+            if (choose == 1) {
+                ClientRmi rmiClientRmi = new ClientRmi();
+                rmiClientRmi.startRmiClient();
+            }
+            if(choose == 2){
+                //do something
+            }
+
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
